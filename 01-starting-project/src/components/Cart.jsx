11ae6416381a8018b88af5dda43478910396e4aa
roxanap33 <roxana.ciocian@principal33.com>
2024-03-15 13:51:58 +0200

    168: Link context to the shopping cart state and handle items addition through it

--- 01-starting-project/src/components/Cart.jsx
+++ 01-starting-project/src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 
-export default function Cart({ items, onUpdateItemQuantity }) {
+export default function Cart({ onUpdateItemQuantity }) {
   const { items } = useContext(CartContext);
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
